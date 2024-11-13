dafb8ba1ea837e56392170bd8542709cc8bb27c2
Ell3Nev3ss <00001099686015sp@al.educacao.sp.gov.br>
2024-11-13 07:56:19 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Supernatural temporadas',
+    'Em que temporada Sam perde a alma?',
+    'Na 6ª temporada'
+)
+
+criaCartao(
+    'Genshin Impact',
+    'Em que ano foi lançado?',
+    '28 de setembro de 2020'
+)
+
+criaCartao(
+    'League of Legends',
+    'Melhor personagem para iniciantes?',
+    'Annie costuma ser considerada uma da Campeãs mais fáceis de usar no League of Legends'
+)
+
+criaCartao(
+    'Supernatural',
+    'Quais são os personagens principais?',
+    'Dean e Sam Winchester'
+)
